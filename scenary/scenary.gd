2d8510da2018565316e14d14a63f629783b998c9
basanez_erik <basanez_erik@eitb.eus>
2024-06-03 10:46:18 +0200

    zoom out

--- scenary/scenary.gd
+++ scenary/scenary.gd
@@ -4,6 +4,8 @@ var cam_speed = 0.06
 var floor
 var scenary
 var cam1
+var max_zoom_out = 4.0
+var max_zoom_in = 0.7
 
 func _ready():
 	floor = self.get_parent()
@@ -18,9 +20,9 @@ func _on_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 && event.pressed == false: #click
 			_on_click(position)
-		if event.button_index == 4  && cam1.position.y < 4: #roll up
+		if event.button_index == 4  && cam1.position.y < max_zoom_out: #roll up
 			cam1.position.y += cam_speed
-		if event.button_index == 5 && cam1.position.y > 0.7: #roll down
+		if event.button_index == 5 && cam1.position.y > max_zoom_in: #roll down
 			cam1.position.y -= cam_speed
 			
 func _on_click(position):
